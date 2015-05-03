5e11ae7ce364db3544a4dbbf840121ec59c9e1f6
markasiddao <markasiddao@gmail.com>
2015-05-03 19:46:35 +0800

    add config

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  root 					'up_arise#Home'
+  get 'about'		=>	'up_arise#About'
+  get 'members'		=>	'up_arise#Members'
+  get 'events'		=>	'up_arise#Events'
+  get 'contact'		=>	'up_arise#Contact'
+  get 'signup'		=>	'users#new'
+  put 'role'		=>  'users#update'
+  get 'login'		=>	'sessions#new'
+  post 'login'		=>	'sessions#create'
+  delete 'logout'	=>	'sessions#destroy'
+  
+  resources :users 
+
+
+end
